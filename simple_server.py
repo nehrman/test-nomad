4632b2bd4831960a1cc285195f03df6f36b10bd4
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-08-19 18:41:27 +0200

    Update simple_server.py

--- simple_server.py
+++ simple_server.py
@@ -4,14 +4,15 @@ import socketserver
 import os
 
 class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_my_headers()
-        http.server.SimpleHTTPRequestHandler.end_headers(self)
+   def end_headers(self):
+        self.send_my_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
 
-    def send_my_headers(self):
-        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
-        self.send_header("Pragma", "no-cache")
-        self.send_header("Expires", "0")
+   def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
 
 with socketserver.TCPServer((os.environ['NOMAD_IP_toto'], int(os.environ['NOMAD_PORT_toto'])), MyHTTPRequestHandler) as httpd:
-    httpd.serve_forever()
+    httpd.serve_forever()
+
